18a3286580fff50a8f62ab940bb1c1b6a24d823d
xunaid5 <junaid.cou@gmail.com>
2022-02-02 19:27:09 +0600

    icons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { RiFacebookCircleFill,RiTwitterFill,RiInstagramFill } from "react-icons/ri";
 import './Footer.css'
 const Footer = () => {
     return (
@@ -20,27 +21,25 @@ const Footer = () => {
   	 			<h4>get help</h4>
   	 			<ul>
   	 				<li><Link to="#">FAQ</Link></li>
-  	 				<li><Link to="#">shipping</Link></li>
-  	 				<li><Link to="#">returns</Link></li>
-  	 				<li><Link to="#">order status</Link></li>
-  	 				<li><Link to="#">payment options</Link></li>
+  	 				<li><Link to="/shipping">shipping</Link></li>
+  	 				
+  	 				<li><Link to="/orders">order status</Link></li>
+  	 				
   	 			</ul>
   	 		</div>
   	 		<div className="footer-col">
   	 			<h4>online shop</h4>
   	 			<ul>
-  	 				<li><Link to="#">watch</Link></li>
-  	 				<li><Link to="#">bag</Link></li>
-  	 				<li><Link to="#">shoes</Link></li>
-  	 				<li><Link to="#">dress</Link></li>
+  	 				<li><Link to="/">Products</Link></li>
+  	 				
   	 			</ul>
   	 		</div>
   	 		<div className="footer-col">
   	 			<h4>follow us</h4>
   	 			<div className="social-links">
-  	 				<Link to="#"><i className="fab fa-facebook-f"></i></Link>
-  	 				<Link to="#"><i className="fab fa-twitter"></i></Link>
-  	 				<Link to="#"><i className="fab fa-instagram"></i></Link>
+  	 				<Link to="#"><RiFacebookCircleFill/></Link>
+  	 				<Link to="#"><RiTwitterFill/></Link>
+  	 				<Link to="#"><RiInstagramFill/></Link>
   	 				{/* <Link to="#"><i className="fab fa-linkedin-in"></i></Link> */}
   	 			</div>
   	 		</div>
